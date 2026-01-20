23c36806e0744af4b564f1b213b6d53f31e779da
Fakrul Hasan Sajib <ahmedsajib1999s@gmail.com>
2026-01-20 19:08:08 +0600

    list exercise

---
+++ ListExercise.py
@@ -0,0 +1,17 @@
+
+#print the second item in the list
+fruits = ['apple' ,'banana' ,'mango']
+print(fruits[1] )
+#change the value from "apple" to "kiwi" in the fruit list
+fruits[0] = 'kiwi'
+print(fruits)
+#Select the missing parts to add the elements of tropical to fruits:
+fruits = ['apple', 'banana', 'cherry']
+tropical = ['mango', 'pineapple', 'papaya']
+fruits.extend(tropical)
+#Insert the missing part of the while loop below to loop through the items of a list.
+mylist =['apple','banana','cherry']
+i=0
+while i <len(mylist):
+    print(mylist[i])
+    i=i+1
