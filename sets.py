243dc1fd9c9f33f46652fbd41101b184b20d8e22
Fakrul Hasan Sajib <ahmedsajib1999s@gmail.com>
2026-01-24 21:30:31 +0600

    sets remove and union

--- sets.py
+++ sets.py
@@ -5,3 +5,10 @@ print(len(mysets))
 #access the set item
 for i in mysets:
     print(i)
+#remove set items
+mysets ={1,2,3,4,5,6}
+mysets.remove(4)
+print(mysets)
+set1 = {"a","b","c"}
+set2 ={ 1,2,3}
+print(set1.union(set2))
