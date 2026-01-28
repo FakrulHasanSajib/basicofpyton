b577974f0b59342cd1ca2bd48b8c0abf8528f4e8
Fakrul Hasan Sajib <ahmedsajib1999s@gmail.com>
2026-01-28 22:52:56 +0600

    Dictonaries

---
+++ Dictonories.py
@@ -0,0 +1,19 @@
+studentinfo ={
+    "sajib" :{
+        "Location" :"Tangail",
+        "Study": "University",
+        "StudentId": 201902103,
+        "Number": 1798585333
+
+    },
+    "Hasan": {
+        "Location": "Tangail",
+        "Study": "University",
+        "StudentId": 201902103,
+        "Number": 1798585333
+
+    }
+
+}
+print(studentinfo)
+print(studentinfo["sajib"]["Location"])
