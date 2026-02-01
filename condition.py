622ddc744e1f065a44d27bea334d2dd134a700b6
Fakrul Hasan Sajib <ahmedsajib1999s@gmail.com>
2026-02-01 21:57:36 +0600

    2 task of hackerrank

--- condition.py
+++ condition.py
@@ -6,3 +6,22 @@ elif a == b:
     print(" duitai soman  ")
 else :
     print('a b er theke boro na ')
+# task hacker ranks
+n = int(input().strip())
+
+if n % 2 == 1:
+    print("Weird")
+elif 2 <= n <= 5:
+    print("Not Weird")
+elif 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
+
+#task 2
+a = int(input())
+b = int(input())
+
+print(a + b)   # sum
+print(a - b)   # difference
+print(a * b)
