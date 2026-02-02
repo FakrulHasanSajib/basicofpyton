b6729f60954396588daeb5226dc10063111b4e3e
Fakrul Hasan Sajib <ahmedsajib1999s@gmail.com>
2026-02-02 19:41:16 +0600

    loop

---
+++ loop.py
@@ -0,0 +1,12 @@
+#while loop
+sajib = 0
+while sajib < 10:
+    print("yes ")
+    sajib = sajib +1
+#for loop
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+    print(x)
